8f29744175c888d7d6d72c08ea4a3a3111e54217
ANBonin <adriano.bonin@outlook.com>
2025-12-08 13:30:27 -0300

    Updated license headers

--- src/main/java/JacocoReporter.java
+++ src/main/java/JacocoReporter.java
@@ -1,10 +1,30 @@
+/*
+ Copyright 2025 Adriano Naime Bonin
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+
 import org.jacoco.core.analysis.*;
 import org.jacoco.core.data.*;
 import org.jacoco.core.tools.ExecFileLoader;
 
 import java.io.*;
 
-
+/**
+ * This is the primary class that deals with the deserialization of JaCoCo's binaries and the reporting
+ * @author Adriano Naime Bonin (alias c3nt4ur1)
+ */
 public class JacocoReporter {
 
     static String execFile = "target/jacoco.exec";
