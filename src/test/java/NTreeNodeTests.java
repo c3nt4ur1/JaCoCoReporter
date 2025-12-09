674d80b25535bbf6df26046ca014f779aa4e0879
ANBonin <adriano.bonin@outlook.com>
2025-12-09 14:48:35 -0300

    Setup testing environment for the n-ary tree structure

---
+++ src/test/java/NTreeNodeTests.java
@@ -0,0 +1,54 @@
+/*
+ Copyright 2025 Adriano Naime Bonin
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import resources.NTreeNode;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import org.jacoco.core.analysis.ICoverageNode;
+import org.jacoco.core.analysis.IBundleCoverage;
+
+public class NTreeNodeTests<T> {
+    IBundleCoverage bundle;
+    NTreeNode<ICoverageNode> rootNode;
+
+    @BeforeEach
+    void setup(){
+        try {
+            bundle = JacocoReporter.Analyse(JacocoReporter.deserealize());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Generates a tree with the bundle only.
+        rootNode = new NTreeNode<>(bundle, null);
+
+    }
+
+    @AfterEach
+    void wrapUp(){
+        bundle = null;
+        rootNode = null;
+    }
+
+}
